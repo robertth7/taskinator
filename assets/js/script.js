12c362c5eb84d370d754d4075e391cacb23ba191
Roberto Hurtado Jr <rhurtado.267@gmail.com>
2023-08-01 18:34:30 -0500

    status of task save

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,6 @@ var createTaskEl = function(taskDataObj) {
     // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-
     // add task id as a custom attribute
     listItemEl.setAttribute("data-task-id", taskIdCounter);
 
@@ -51,17 +50,33 @@ var createTaskEl = function(taskDataObj) {
     var taskInfoEl = document.createElement("div");
     // give it a class name
     taskInfoEl.className = "task-info";
-    
     // add HTML content to div
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-
     listItemEl.appendChild(taskInfoEl);
 
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
-
     // add entire list item to list
-    tasksToDoEl.appendChild(listItemEl);
+
+    switch (taskDataObj.status) {
+        case "to do":
+            taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.append(listItemEl);
+            break;
+        case "in progress":
+            taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.append(listItemEl);
+            break;
+        case "completed":
+            taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.append(listItemEl);
+            break;
+        default:
+            console.log("Something went wrong!");
+    }
+
+
+    //tasksToDoEl.appendChild(listItemEl);
 
     //console.log(taskDataObj);
     //console.log(taskDataObj.status);
@@ -236,11 +251,14 @@ var saveTasks = function () {
 var loadTasks = function () {
     // get task items from localStorage()
     var savedTasks = localStorage.getItem("tasks");
-
+    // if there are no tasks, set tasks to an empty array and return out of the function
     if (!savedTasks) {
         return false;
     }
+    console.log("Saved tasks found!")
+    // else, load up saved tasks
 
+    // parse into array of objects
     savedTasks = JSON.parse(savedTasks);
 
     // loop through savedTasks array
@@ -248,7 +266,7 @@ var loadTasks = function () {
         // pass each task object into the 'createTaskEl()' function
         createTaskEl(savedTasks[i]);
     }
-}
+};
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
